dcd1482ab93daa5ab500df6c6a4783cfbcf7716c
zhanbao2000 <zhanbao2000@163.com>
2019-02-16 15:22:13 +0800

    添加了注释

--- checksame.py
+++ checksame.py
@@ -3,6 +3,8 @@
 
 import os, codecs
 
+#使用os.getcwdu()摆脱烦人的编码问题
+#获取到两个目录下的所有文件
 path1 = raw_input("Please enter path 1: ")
 os.chdir(path1)
 songs1 = os.listdir(os.getcwdu())
@@ -10,12 +12,12 @@ path2 = raw_input("Please enter path 2: ")
 os.chdir(path2)
 songs2 = os.listdir(os.getcwdu())
 
-
-fo = codecs.open("H:\Developer\Python\A35Helper\same.txt", "w", "utf-8")
+fo = codecs.open("same.txt", "w", "utf-8")
 
 fo.writelines(path1 + "\n")
-fo.writelines(path2 + "\n")
+fo.writelines(path2 + "\n\n")
 
+#逐个对照，发现重复的就输出
 for i in range(0,len(songs1)):
 	for j in range(0,len(songs2)):
 		if songs1[i] == songs2[j]:
--- m3uMaker.py
+++ m3uMaker.py
@@ -3,13 +3,16 @@
 
 import os, codecs
 
+#使用os.getcwdu()摆脱烦人的编码问题
 songs = os.listdir(os.getcwdu())
 fo = codecs.open("1.m3u", "w", "utf-8")
 
 fo.writelines("#EXTM3U\n")
 
 for i in range(0,len(songs)):
+	#检查是不是mp3文件
 	if songs[i][-4:] == ".mp3":
+		#写入列表
 		fo.writelines("#EXTINF:,\n")
 		fo.writelines(songs[i] + "\n")
 
