af808b9a946403f581f67f84b0e4621390c5e7e2
zhanbao2000 <zhanbao2000@163.com>
2019-02-16 15:42:34 +0800

    1.0.1 标记了只允许使用英文路径

--- checksame.py
+++ checksame.py
@@ -5,12 +5,15 @@ import os, codecs
 
 #使用os.getcwdu()摆脱烦人的编码问题
 #获取到两个目录下的所有文件
+print ("English path ONLY!!")
 path1 = raw_input("Please enter path 1: ")
 os.chdir(path1)
 songs1 = os.listdir(os.getcwdu())
+print ("Load %d files" % len(songs1))
 path2 = raw_input("Please enter path 2: ")
 os.chdir(path2)
 songs2 = os.listdir(os.getcwdu())
+print ("Load %d files" % len(songs2))
 
 fo = codecs.open("same.txt", "w", "utf-8")
 
