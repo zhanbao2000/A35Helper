064b53f0413c35f8bb118dbf6818447bd8c6f1d9
zhanbao2000 <zhanbao2000@163.com>
2019-02-16 14:51:59 +0800

    原始版本

--- m3uMaker.py
+++ m3uMaker.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
 # coding:utf-8
+
+import os, codecs
+
+songs = os.listdir(os.getcwdu())
+fo = codecs.open("1.m3u", "w", "utf-8")
+
+fo.writelines("#EXTM3U\n")
+
+for i in range(0,len(songs)):
+	if songs[i][-4:] == ".mp3":
+		fo.writelines("#EXTINF:,\n")
+		fo.writelines(songs[i] + "\n")
+
+fo.close()
+
