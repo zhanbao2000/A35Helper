7e3c1811918df7c018f2376bfb4b976412a83ce0
zhanbao2000 <zhanbao2000@163.com>
2019-02-20 01:08:05 +0800

    0.0.5 编写中......为防止再次手贱，先上传一次

--- lrc_rebuild.py
+++ lrc_rebuild.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python
 # coding:utf-8
-# ver 0.0.1
+# ver 0.0.5
 
 import os, codecs
+
+test_path = u"H:\\Developer\\Python\\A35Helper\\testfile\\Poppin'Party - STAR BEAT!～ホシノコドウ～.lrc"
+
+def gettime(char):
+	try:
+		result = int(char[1:3])*60 + float(char[4:9])
+	except:
+		result = -1
+	return result
+
+def rebuild(file_path):
+	fr = codecs.open(file_path, "r", "utf-8")
+	lrcs = [line.strip() for line in fr] 
+	fr.close()
+	
+	i = 0
+	while i < len(lrcs)-1:
+		if gettime(lrcs[i]) > gettime(lrcs[i+1]):
+			temp = lrcs.pop(i+1)
+			j = 0
+			while gettime(temp) >= gettime(lrcs[j]): j += 1
+			lrcs.insert(j, temp)
+			i = 0
+			continue
+		i += 1
+	
+	fo = codecs.open(file_path, "w", "utf-8")
+	i = 0
+	while i <= len(lrcs):
+		if lrcs[i] == u"":
+			lrcs.pop(i)
+			i += 1
+			continue
+		fo.writelines(lrcs[i] + "\n")
+		i += 1
+		
+rebuild(test_path)
